d9a211291a440dbb76ddfe1fc234411b50524aae
timakaa3 <167969556+timakaa3@users.noreply.github.com>
2026-02-17 22:16:07 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,249 @@
+#include <Bluepad32.h>
+#include <ESP32Servo.h>
 
+ControllerPtr myControllers[BP32_MAX_GAMEPADS];
+
+// ---- SERVO ----
+Servo servo;
+static const int SERVO_PIN = 14;
+static int servoAngle = 90;
+
+// This callback gets called any time a new gamepad is connected.
+// Up to 4 gamepads can be connected at the same time.
+void onConnectedController(ControllerPtr ctl) {
+    bool foundEmptySlot = false;
+    for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+        if (myControllers[i] == nullptr) {
+            Serial.printf("CALLBACK: Controller is connected, index=%d\n", i);
+
+            ControllerProperties properties = ctl->getProperties();
+            Serial.printf("Controller model: %s, VID=0x%04x, PID=0x%04x\n",
+                          ctl->getModelName().c_str(),
+                          properties.vendor_id,
+                          properties.product_id);
+
+            myControllers[i] = ctl;
+            foundEmptySlot = true;
+            break;
+        }
+    }
+    if (!foundEmptySlot) {
+        Serial.println("CALLBACK: Controller connected, but could not found empty slot");
+    }
+}
+
+void onDisconnectedController(ControllerPtr ctl) {
+    bool foundController = false;
+
+    for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+        if (myControllers[i] == ctl) {
+            Serial.printf("CALLBACK: Controller disconnected from index=%d\n", i);
+            myControllers[i] = nullptr;
+            foundController = true;
+            break;
+        }
+    }
+
+    if (!foundController) {
+        Serial.println("CALLBACK: Controller disconnected, but not found in myControllers");
+    }
+}
+
+void dumpGamepad(ControllerPtr ctl) {
+    Serial.printf(
+        "idx=%d, dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, %4d, brake: %4d, throttle: %4d, "
+        "misc: 0x%02x, gyro x:%6d y:%6d z:%6d, accel x:%6d y:%6d z:%6d\n",
+        ctl->index(),        // Controller Index
+        ctl->dpad(),         // D-pad
+        ctl->buttons(),      // bitmask of pressed buttons
+        ctl->axisX(),        // (-511 - 512) left X Axis
+        ctl->axisY(),        // (-511 - 512) left Y axis
+        ctl->axisRX(),       // (-511 - 512) right X axis
+        ctl->axisRY(),       // (-511 - 512) right Y axis
+        ctl->brake(),        // (0 - 1023): brake button
+        ctl->throttle(),     // (0 - 1023): throttle (AKA gas) button
+        ctl->miscButtons(),  // bitmask of pressed "misc" buttons
+        ctl->gyroX(),        // Gyro X
+        ctl->gyroY(),        // Gyro Y
+        ctl->gyroZ(),        // Gyro Z
+        ctl->accelX(),       // Accelerometer X
+        ctl->accelY(),       // Accelerometer Y
+        ctl->accelZ()        // Accelerometer Z
+    );
+}
+
+void dumpMouse(ControllerPtr ctl) {
+    Serial.printf("idx=%d, buttons: 0x%04x, scrollWheel=0x%04x, delta X: %4d, delta Y: %4d\n",
+                   ctl->index(),
+                   ctl->buttons(),
+                   ctl->scrollWheel(),
+                   ctl->deltaX(),
+                   ctl->deltaY()
+    );
+}
+
+void dumpKeyboard(ControllerPtr ctl) {
+    static const char* key_names[] = {
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+        "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+        "Enter", "Escape", "Backspace", "Tab", "Spacebar", "Underscore", "Equal", "OpenBracket", "CloseBracket",
+        "Backslash", "Tilde", "SemiColon", "Quote", "GraveAccent", "Comma", "Dot", "Slash", "CapsLock",
+        "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12",
+        "PrintScreen", "ScrollLock", "Pause", "Insert", "Home", "PageUp", "Delete", "End", "PageDown",
+        "RightArrow", "LeftArrow", "DownArrow", "UpArrow",
+    };
+    static const char* modifier_names[] = {
+        "Left Control", "Left Shift", "Left Alt", "Left Meta",
+        "Right Control", "Right Shift", "Right Alt", "Right Meta",
+    };
+
+    Serial.printf("idx=%d, Pressed keys: ", ctl->index());
+    for (int key = Keyboard_A; key <= Keyboard_UpArrow; key++) {
+        if (ctl->isKeyPressed(static_cast<KeyboardKey>(key))) {
+            const char* keyName = key_names[key - 4];
+            Serial.printf("%s,", keyName);
+       }
+    }
+    for (int key = Keyboard_LeftControl; key <= Keyboard_RightMeta; key++) {
+        if (ctl->isKeyPressed(static_cast<KeyboardKey>(key))) {
+            const char* keyName = modifier_names[key - 0xe0];
+            Serial.printf("%s,", keyName);
+        }
+    }
+    Console.printf("\n");
+}
+
+void dumpBalanceBoard(ControllerPtr ctl) {
+    Serial.printf("idx=%d,  TL=%u, TR=%u, BL=%u, BR=%u, temperature=%d\n",
+                   ctl->index(),
+                   ctl->topLeft(),
+                   ctl->topRight(),
+                   ctl->bottomLeft(),
+                   ctl->bottomRight(),
+                   ctl->temperature()
+    );
+}
+
+void processGamepad(ControllerPtr ctl) {
+    // ---- SERVO CONTROL (LEFT STICK X) ----
+    // axisX: (-511..512) -> angle (0..180)
+    int x = ctl->axisX();
+
+    const int DEADZONE = 35;
+    if (abs(x) < DEADZONE) x = 0;
+
+    int angle = map(x, -511, 512, 0, 180);
+    if (angle < 0) angle = 0;
+    if (angle > 180) angle = 180;
+
+    // Square (X на PS4) центрира
+    if (ctl->x()) angle = 90;
+
+    servoAngle = angle;
+    servo.write(servoAngle);
+
+    // ---- ORIGINAL EXAMPLE FEATURES ----
+    if (ctl->a()) {
+        static int colorIdx = 0;
+        switch (colorIdx % 3) {
+            case 0: ctl->setColorLED(255, 0, 0); break;
+            case 1: ctl->setColorLED(0, 255, 0); break;
+            case 2: ctl->setColorLED(0, 0, 255); break;
+        }
+        colorIdx++;
+    }
+
+    if (ctl->b()) {
+        static int led = 0;
+        led++;
+        ctl->setPlayerLEDs(led & 0x0f);
+    }
+
+    // Rumble (пример) – оставям го
+    if (ctl->x()) {
+        ctl->playDualRumble(0, 250, 0x80, 0x40);
+    }
+
+    dumpGamepad(ctl);
+}
+
+void processMouse(ControllerPtr ctl) {
+    if (ctl->scrollWheel() > 0) {
+        // Do Something
+    } else if (ctl->scrollWheel() < 0) {
+        // Do something else
+    }
+    dumpMouse(ctl);
+}
+
+void processKeyboard(ControllerPtr ctl) {
+    if (!ctl->isAnyKeyPressed())
+        return;
+
+    if (ctl->isKeyPressed(Keyboard_A)) {
+        Serial.println("Key 'A' pressed");
+    }
+
+    if (ctl->isKeyPressed(Keyboard_LeftShift)) {
+        Serial.println("Key 'LEFT SHIFT' pressed");
+    }
+
+    if (ctl->isKeyPressed(Keyboard_LeftArrow)) {
+        Serial.println("Key 'Left Arrow' pressed");
+    }
+
+    dumpKeyboard(ctl);
+}
+
+void processBalanceBoard(ControllerPtr ctl) {
+    if (ctl->topLeft() > 10000) {
+        // Do Something
+    }
+    dumpBalanceBoard(ctl);
+}
+
+void processControllers() {
+    for (auto myController : myControllers) {
+        if (myController && myController->isConnected() && myController->hasData()) {
+            if (myController->isGamepad()) {
+                processGamepad(myController);
+            } else if (myController->isMouse()) {
+                processMouse(myController);
+            } else if (myController->isKeyboard()) {
+                processKeyboard(myController);
+            } else if (myController->isBalanceBoard()) {
+                processBalanceBoard(myController);
+            } else {
+                Serial.println("Unsupported controller");
+            }
+        }
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
+    const uint8_t* addr = BP32.localBdAddress();
+    Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+
+    BP32.setup(&onConnectedController, &onDisconnectedController);
+
+    // Ако искаш да се reconnect-ва след като веднъж се сдвои,
+    // закоментирай следващия ред:
+    //BP32.forgetBluetoothKeys();
+
+    BP32.enableVirtualDevice(false);
+
+    // ---- SERVO INIT ----
+    servo.setPeriodHertz(50);        // 50Hz за SG90
+    servo.attach(SERVO_PIN, 500, 2400);
+    servo.write(servoAngle);
+}
+
+void loop() {
+    bool dataUpdated = BP32.update();
+    if (dataUpdated)
+        processControllers();
+
+    delay(10);
+}
